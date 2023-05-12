fd0c4ee9a658aad7fca2845d4ea99949a23a572f
Brandon <brandonamorgan@gmail.com>
2023-05-12 15:24:03 -0700

    new branch

--- src/main/java/controller/DungeonApp.java
+++ src/main/java/controller/DungeonApp.java
@@ -1,4 +1,12 @@
 package controller;
 
-public class DungeonApp {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class DungeonApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
 }
