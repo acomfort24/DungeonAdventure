8fc06f319fd91ea784254d148f6e35ebbdcce535
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2023-05-29 21:22:06 -0700

    Can now decrement and increment the inventory model

--- src/main/java/controller/InventoryController.java
+++ src/main/java/controller/InventoryController.java
@@ -1,21 +1,31 @@
 package controller;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
+import com.almasb.fxgl.inventory.Inventory;
+import com.almasb.fxgl.inventory.view.InventoryListView;
+import model.components.PlayerComponent;
 import java.util.HashMap;
+import java.util.*;
+import java.util.concurrent.*;
 
-public class InventoryController extends SceneSwapController {
+public class InventoryController<I,D> extends SceneSwapController {
     public static void useItem(final String theItem) {
         System.out.println("You used a " + theItem);
-        //decrement quantity of item in model
+        Inventory inventory = PlayerComponent.getMyInventory();
+        inventory.remove(theItem);
     }
     public static void addItem(final String theItem) {
-        //increment quantity of item in model
+        Inventory inventory = PlayerComponent.getMyInventory();
+        inventory.add(theItem);
     }
     public static HashMap<String, Integer> getInventory() {
-        HashMap<String, Integer> inventory = new HashMap<>();
-        inventory.put("Health Potion", 4);
-        inventory.put("Vision Potion", 1);
-        return inventory;
+        HashMap<String, Integer> returnedMap = new HashMap<String, Integer>();
+        Inventory inventory = PlayerComponent.getMyInventory();
+        if (!inventory.getAllData().isEmpty()) {
+            System.out.println(inventory);
+//            for (I item: inventory.getAllData()) {
+//                returnedMap.put(I.getName(), inventory.get(I));
+//            }
+        }
+        return returnedMap;
     }
 }
